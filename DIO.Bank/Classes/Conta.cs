72f116134c83a580e967e15ce2687a14445d5d1e
Pietro Alves <pietrorodrigues@gmail.com>
2021-03-11 16:46:35 -0300

    Add project files.

---
+++ DIO.Bank/Classes/Conta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Bank
+{
+	class Conta
+	{		
+		private TipoConta TipoConta { get; set; }
+		private double Saldo { get; set; }
+		private double Credito { get; set; }
+		private string Nome { get; set; }
+
+		public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+		{
+			TipoConta = tipoConta;
+			Saldo = saldo;
+			Credito = credito;
+			Nome = nome;
+		}
+
+		public bool Sacar(double valorSaque)
+		{ // Validar saldo da conta
+			if (this.Saldo - valorSaque < (this.Credito * -1))
+			{
+				Console.WriteLine("Saldo Insuficiente");
+				return false;
+			}
+			this.Saldo -= valorSaque;
+			Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+
+			return true;
+		}
+
+		public void Depositar(double valorDeposito)
+		{
+			this.Saldo += valorDeposito;
+
+			Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+		}
+
+		public void Transferir(double valorTranferencia, Conta contaDestino)
+		{
+			if (this.Sacar(valorTranferencia))
+			{
+				contaDestino.Depositar(valorTranferencia);
+			}
+		}
+
+		public override string ToString()
+		{
+			string retorno = "";
+			retorno += "TipoConta.: " + this.TipoConta + " | ";
+			retorno += "Nome.: " + this.Nome + " | ";
+			retorno += "Saldo.: R$ " + this.Saldo + " | ";
+			retorno += "Credito.: R$ " + this.Credito + " | ";
+			return retorno;
+		}
+	}
+}
---
+++ DIO.Bank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string opcaoUsuario = ObterOpcaoUsuario();
+
+			//Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Pietro Alves");
+			//Console.WriteLine(minhaConta.ToString());			
+		}
+
+		private static string ObterOpcaoUsuario()
+		{
+			Console.WriteLine();
+			Console.WriteLine("DIO Bank ao seu dispor!");
+			Console.WriteLine("Informe a Opção desejada:");
+
+			Console.WriteLine("1 - Listar Conta(s)");
+			Console.WriteLine("2 - Inserir Nova Conta");
+			Console.WriteLine("3 - Transferir");
+			Console.WriteLine("4 - Sacar");
+			Console.WriteLine("5 - Depositar");
+			Console.WriteLine("C - Limpar Tela");
+			Console.WriteLine("X - Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
+		}
+	}
+}
